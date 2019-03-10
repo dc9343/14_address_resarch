158aca24972e3395cf7538e46ebc1ea62015719b
DC <just-one@hotmail.co.jp>
2019-03-10 16:30:10 +0900

    辞書を更新した

---
+++ dict_practice.py
@@ -0,0 +1,27 @@
+address_data = [{"address1": "岩手県",
+                 "address2": "八幡平市",
+                 "address3": "大更",
+                 "zipcode": "0287111"},
+
+                {"address1": "兵庫県",
+                 "address2": "西宮市",
+                 "address3": "高須町",
+                 "zipcode": "6638141"},
+                ]
+
+if __name__ == '__main__':
+    # 課題1 '兵庫県西宮市高須市' と出力してください
+
+    address1 = address_data[1]["address1"]
+    address2 = address_data[1]["address2"]
+    address3 = address_data[1]["address3"]
+
+    print(f"{address1}{address2}{address3}")
+
+    # 課題2 '岩手県八幡平大更' と出力してください
+
+    address1 = address_data[0]["address1"]
+    address2 = address_data[0]["address2"]
+    address3 = address_data[0]["address3"]
+
+    print(f"{address1}{address2}{address3}")
