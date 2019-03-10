18a3500e9abf5054b20aa21fec9b83366d08fee6
DC <just-one@hotmail.co.jp>
2019-03-10 17:56:46 +0900

    入力した郵便番号から住所を出力できるようにした

--- address_search.py
+++ address_search.py
@@ -1,7 +1,10 @@
 import requests
 
 if __name__ == '__main__':
-    zipcode = "1200012"
+
+    zipcode = input("郵便番号(7ケタ)?: ")
+
+    # zipcode = "1200012"
 
     url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
